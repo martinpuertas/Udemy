b9a6bd6eb48039234d9c082b0bc13f110c96e7f4
Martin Puertas <mpuertas@oneboxtds.com>
2022-11-28 09:52:23 +0100

    TD-forms exercise

--- 15.forms-td-assignment-start/src/app/app.component.ts
+++ 15.forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
 
+  mail = '';
+  password = '';
+  subscriptions = ['Basic', 'Advanced', 'Pro']
+  subscriptionType = 'Advanced';
+
+  onSubmit() {
+    console.log(this.form);
+  }
 }
